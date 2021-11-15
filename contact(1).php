e5f9f1f7f2f9ae08e21544b30c50cc0e60c5f09f
Veronika Pshenytska <82585726+Pshenytska@users.noreply.github.com>
2021-11-15 12:01:36 -0800

    Add files via upload

---
+++ contact(1).php
@@ -0,0 +1,12 @@
+
+    <?php
+include('config.php');
+include('includes/header.php'); ?>
+    <?php
+    include('includes/form.php'); ?>
+    <aside>
+        <p>Did you know there is a world outside of your small, all too familiar home? Travel is important to learn more about the world and to experience life in a different environment. So much can be learned and discovered during a trip overseas. It doesn’t matter where the individual travels as long as the desired location is not too close to home so that you can be immersed in another culture.</p>
+        <iframe src="https://giphy.com/embed/toelXGUsYD6vtCN408" width="480" height="360" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+        <p><a href="https://giphy.com/gifs/travel-holiday-journey-toelXGUsYD6vtCN408">via GIPHY</a></p>
+    </aside>
+<?php include('includes/footer.php'); ?>
